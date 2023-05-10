ee2fc2a7833360bd42f78953a6e6f1b91dee3d7e
Akash Mukherjee <akash3.11.2000@gmail.com>
2023-05-10 17:18:06 +0530

    logo image glitch resolved

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,4 @@
+import logo from "../assets/logo.jpg";
 export default function Logo({ playing }) {
   return (
     <a
@@ -9,11 +10,7 @@ export default function Logo({ playing }) {
       }}
       className="hidden md:block w-16 h-16 absolute top-8 left-8 z-50 rounded-full overflow-hidden outline outline-1 outline-white outline-offset-4 animate-spin-slow hover:[scale:1.05] transition-all duration-300"
     >
-      <img
-        className="w-full h-full object-cover"
-        src="/images/logo.jpg"
-        alt="logo"
-      />
+      <img className="w-full h-full object-cover" src={logo} alt="logo" />
     </a>
   );
 }
